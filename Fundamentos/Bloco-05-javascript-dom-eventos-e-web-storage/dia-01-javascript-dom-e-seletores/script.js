0397fd370a51fbe8ad3b6bf46014bdd26603c227
Kerly Sousa <kerlygee@gmail.com>
2021-12-11 19:12:00 -0300

    Altera todas as cores de fundo das tags h3 da section que possui a class emergency-task

--- Fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -3,5 +3,12 @@ cabeçalho.style.backgroundColor = "lightgreen";
 let section = document.querySelectorAll("section");
 section[0].style.backgroundColor = "pink";
 section[1].style.backgroundColor = "orange";
+function alteraCor(elemento, cor){
+  let title = document.querySelectorAll(elemento);
+  for ( let i = 0; i < title.length; i +=1){
+    title[i].style.backgroundColor = "purple";
+  }
+  }
+  alteraCor(".emergency-tasks h3","")
 
 
