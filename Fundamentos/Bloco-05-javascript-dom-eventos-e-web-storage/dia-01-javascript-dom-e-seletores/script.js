0858b1192d04eda3a3b21ddfe3498cda3cb9ed00
Kerly Sousa <kerlygee@gmail.com>
2021-12-11 19:09:35 -0300

    Alteracao da cor de fundo da tag section

--- Fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
+++ Fundamentos/Bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/script.js
@@ -1,5 +1,7 @@
 let cabeçalho = document.querySelector("#header-container");
 cabeçalho.style.backgroundColor = "lightgreen";
-
+let section = document.querySelectorAll("section");
+section[0].style.backgroundColor = "pink";
+section[1].style.backgroundColor = "orange";
 
 
